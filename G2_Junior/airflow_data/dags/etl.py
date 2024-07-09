51c7bba1bfe94c6d5716605bab439b9502f9645c
AnZhi2003 <AnZhi2003@yandex.ru>
2024-07-09 07:45:55 +0200

    Создан даг по инкрементальной загрузке данных в хранилище

--- G2_Junior/airflow_data/dags/etl.py
+++ G2_Junior/airflow_data/dags/etl.py
@@ -1,93 +1,115 @@
 """Сбор данных из таблиц БД источников (shop-1, shop-2) -> инкрементальное обновление данных в DWH"""
 
-"""ВОПРОСЫ по https://teletype.in/@razvodov_alexey/incremental_loading_scd2:
-1. stg_extract = max (created_at) - окно возможного обновления данных. Что это за окно? Как его определить?
-2. stg_transform и stg_load: ...и загрузить в стейджинг слой, таблица в котором предварительно очищается. 
-Зачем очищается стейджинг слой? Мы считаем, что эти данные уже загружены в финальный слой хранилища?
-3. oda_extract и oda_transform_i: откуда появились данные, которые надо обновить?
-4. oda_transform_u
-5. i, u, d - insert, update, delete?
-"""
-
 import pendulum
-import datetime as dt
 import json
+import logging
+import os
+import pandas as pd
+
 from datetime import datetime, timedelta
 from airflow.decorators import dag, task
 from airflow.models import Variable
-from airflow.hooks.postgres_hook import PostgresHook
-#import logging
+from airflow.operators.python import get_current_context
+from sqlalchemy import create_engine
+from sqlalchemy.engine import Engine
+
+from func_etl import checking_schema_tables_columns_in_DB, get_max_created_at, get_table_from_DB, get_sqlalchemy_engine
 
 """Variables"""
 var_airflow_etl_str = Variable.get("var_airflow_etl")
 var_airflow_etl_dict = json.loads(var_airflow_etl_str)
 var_DWH_tables = var_airflow_etl_dict["tables"]
+var_source_conn = var_airflow_etl_dict["source_conn"]
 
-"""Connections:"""
-DWH_pg_hook = PostgresHook(postgres_conn_id="pg-dwh")
-DWH_conn = DWH_pg_hook.get_conn()
-DWH_cursor = DWH_conn.cursor()
-
-shop_1_pg_hook = PostgresHook(postgres_conn_id="pg-shop-1")
-shop_1_conn = shop_1_pg_hook.get_conn()
-shop_1_cursor = shop_1_conn.cursor()
-
-shop_2_pg_hook = PostgresHook(postgress_conn_id="pg-shop-2")
-shop_2_conn = shop_2_pg_hook.get_conn()
-shop_2_cursor = shop_2_conn.cursor()
-
-def check_schema_in_DWH():
-    """Проверка наличия таблиц в DWH. Если таблицы нет, создать таблицу. 
-    Если в источниках появятся новые таблицы, можно их добавить в переменную, и данные из них также будут перекладываться в хранилище.
-    Можно будет вынести в отдельный файл, так как будет использоваться и для stg, и для dds"""
-    pass
-
-def get_max_created_at():
-    """"Получение максимальной даты по столбцу "created_at" из любой таблицы, как источника, так и таргета.
-    SELECT MAX(created_at) AS max_created_at FROM table_name;
-    Можно будет вынести в отдельный файл"""
-    pass
+"""Connection to DWH"""
+DWH_engine = get_sqlalchemy_engine("pg-dwh")
 
 default_args = {"owner": "anzhi", 
                 "depends_on_past": False,
-                "start_date": dt.datetime(2024, 7, 2, 12, 0, 0, tzinfo=pendulum.timezone("UTC")),
+                "start_date": datetime(2024, 7, 2, 12, 0, 0, tzinfo=pendulum.timezone("UTC")),
                 "retry_delay": timedelta(minutes=5),
                 "retries": 1,
                 }
 
-@dag(default_args=default_args, catchup=False, tags=["etl"])
+@dag(default_args=default_args, catchup=False, tags=["etl"], schedule_interval='*/2 * * * *')
 def etl():
-    
-    @task()
-    def ectract_transform():
-        """
-        1. check_schema_in_DWH()
-        2. get_max_created_at(target_table) as max_created_at_target
-        3. Если max_created_at_target == None, то из source_table получить все данные, что в ней есть. 
-        Если max_created_at_target != None, то данные после max_created_at_target.
-        4. Добавить столбцы 'igested_at', 'city-name', 'id_source' (доп ключ по {table}_id и src_id)
-        5. Сохранить в csv с именем файла 'execution_date_dag_id' в '/tmp/airflow_staging/load_staging_data'
-        6. Вернуть полный путь к файлу
-         load_dds_data
-        """
-        pass
 
     @task()
-    def load():
+    def checking_tables_in_DWH():
+        return checking_schema_tables_columns_in_DB(DWH_engine, "dds", var_DWH_tables)
+    
+    @task()
+    def extract_transform(**context) -> list:
+        """
+        В каждой БД-источнике, в каждой таблице:
+        1. Получить таблицу, в которой created_at > max_created_at_target
+        2. Если max_created_at_target == None, то из source_table получить все данные, что в ней есть. 
+        3. Добавить столбцы 'igested_at', 'city_name', 'id_source' (доп ключ по {table}_id и src_id)
+        4. Сохранить в csv с именем файла 'execution_date_dag_id' в '/tmp/airflow_staging'
+        5. Вернуть полный путь к файлу
+        """
+        context = get_current_context()
+        dag_run_id = context["dag_run"].run_id
+        dag_run_datetime = context["dag_run"].execution_date
+        dag_run_date = context["dag_run"].execution_date.date()
+    
+        list_of_paths = []
+
+        for source_key, source_param in var_source_conn.items():
+            source_name = source_key
+            source_engine = get_sqlalchemy_engine(source_param["hook"])
+            city_name = source_param["city_name"]
+            
+            for table_name in var_DWH_tables.keys():
+                # Максимальная дата в DWH:
+                max_created_at_DWH = get_max_created_at(DWH_engine, "dds", table_name) 
+                # Dataframe после max_created_at_DWH:
+                df_source = get_table_from_DB(source_engine, "public", table_name, max_created_at_DWH) 
+                
+                # Если вернется пустой df:
+                if df_source.empty:
+                    logging.info(f"No new data found for table {table_name}")
+                    continue
+                
+                # Добавляем дополнительные столбцы:
+                df_source["igested_at"] = dag_run_datetime
+                df_source["city_name"] = city_name
+                df_source["id_source"] = df_source[df_source.columns[0]].apply(lambda x: f"{source_name}_{x}")
+
+                # Добавить создание файла и записать путь в виде строки в список list_of_paths
+                file_name = f"{dag_run_id}_{dag_run_date}_{table_name}.csv"
+                file_path = os.path.join('/tmp/airflow_staging', file_name)
+                os.makedirs(os.path.dirname(file_path), exist_ok=True)
+                df_source.to_csv(file_path, sep = ',', index=False, header=True, encoding='utf-8', date_format='%Y-%m-%d %H:%M:%S', float_format='%.2f')
+
+                list_of_paths.append(file_path)
+        
+        return list_of_paths
+
+    @task()
+    def load(list_of_paths, DWH_engine: Engine):
         """
         1. Получить датафрейм из файла, полученного на предыдущем шаге.
         2. df.to_sql()
         """
-        pass
+        schema_name = "dds"
+
+        for file_path in list_of_paths:
+            df = pd.read_csv(file_path)
+            table_name = file_path.split('/')[-1].split('_')[-1].split('.')[0]
+
+            try:
+                df.to_sql(table_name, DWH_engine, schema=schema_name,if_exists='append', index=False)
+                logging.info(f"Data loaded successfully into {schema_name}.{table_name}")
+            except Exception as e:
+                logging.error(f"Error loading data into {schema_name}.{table_name}: {e}")
 
 
+
+    checking_tables_in_DWH_task = checking_tables_in_DWH()
+    extract_transform_task = extract_transform()
+    load_task = load(extract_transform_task, DWH_engine)
+
+    checking_tables_in_DWH_task >> extract_transform_task >> load_task
 
 etl()
-
-
-DWH_conn.close()
-DWH_cursor.close()
-shop_1_conn.close()
-shop_1_cursor.close()
-shop_2_conn.close()
-shop_2_cursor.close()
---
+++ G2_Junior/airflow_data/dags/func_etl.py
@@ -0,0 +1,99 @@
+from airflow.hooks.postgres_hook import PostgresHook
+from sqlalchemy import create_engine, text
+from sqlalchemy.engine import Engine
+from typing import Dict
+from datetime import datetime
+import logging
+import pandas as pd
+
+
+def get_sqlalchemy_engine(conn_id) -> Engine:
+    hook = PostgresHook(postgres_conn_id=conn_id)
+    return create_engine(hook.get_uri())
+
+def checking_schema_tables_columns_in_DB(engine: Engine, schema_name: str, table_dict: Dict[str, Dict[str, str]]) -> bool:
+    # Creating schema:
+    with engine.connect() as conn:
+        conn.execute(text(f"CREATE SCHEMA IF NOT EXISTS {schema_name};"))
+
+        all_tables_created = True
+
+        # Checking and crating tables and columns:
+        for table_name, columns in table_dict.items():
+            # Create tables if not exists
+            result = conn.execute(text(f"""SELECT EXISTS (
+                        SELECT 1
+                        FROM information_schema.tables
+                        WHERE table_schema = :schema_name
+                        AND table_name = :table_name);
+                        """), {"schema_name": schema_name, "table_name": table_name})
+            table_exists = result.scalar()
+
+            if not table_exists:
+                columns_list = ", ".join([f"{col} {typ}" for col, typ in columns.items()])
+                create_table_query = text(f"""CREATE TABLE {schema_name}.{table_name} ({columns_list});""")
+                try:
+                    conn.execute(create_table_query)
+                    logging.info(f"Table {schema_name}.{table_name} created successfully.")
+                except Exception as e:
+                    logging.error(f"Error creating table {schema_name}.{table_name}: {e}.")
+                    all_tables_created = False
+
+            # Adding lacking columns    
+            result = conn.execute(text(f"""
+                        SELECT column_name, data_type
+                        FROM information_schema.columns
+                        WHERE table_schema = :schema_name
+                        AND table_name = :table_name;
+                        """), {"schema_name": schema_name, "table_name": table_name})
+            columns_exists = {row["column_name"]: row["data_type"] for row in result}
+
+            for col, typ in columns.items():
+                if col not in columns_exists:
+                    alter_table_query = text(f"ALTER TABLE {schema_name}.{table_name} ADD COLUMN {col} {typ};")
+
+                    try:
+                        conn.execute(alter_table_query)
+                        logging.info(f"Column added {col} to table {schema_name}.{table_name}.")
+                    except Exception as e:
+                        logging.error(f"Error adding column {col} to table {schema_name}.{table_name}: {e}.")
+                        all_tables_created = False
+        
+    return all_tables_created
+            
+def get_max_created_at(engine: Engine, schema_name: str, table_name: str) -> datetime | None:
+    """"Получение максимальной даты по столбцу "created_at" из любой таблицы, как источника, так и таргета."""
+    with engine.connect() as conn:
+        try:
+            result = conn.execute(text(f"""
+                        SELECT MAX(created_at) AS max_created_at 
+                        FROM {schema_name}.{table_name};
+                        """))
+            max_created_at = result.scalar()
+        except Exception as e:
+            logging.error(f"Error fetching max created_at from {schema_name}.{table_name}: {e}.")
+            max_created_at = None
+
+    return max_created_at
+
+def get_table_from_DB(engine: Engine, schema_name: str, table_name: str, max_created_at: datetime | None) -> pd.DataFrame:
+    with engine.connect() as conn:
+        try:
+            if max_created_at is None:
+                query= text(f"""
+                        SELECT * 
+                        FROM {schema_name}.{table_name};
+                        """)
+            else:
+                query = text(f"""
+                SELECT * 
+                FROM {schema_name}.{table_name}
+                WHERE created_at > ':max_created_at;
+                """)
+            df = pd.read_sql_query(query, conn, params={"max_created_at": max_created_at})
+
+        except Exception as e:
+            logging.error(f"Error fetching data from {schema_name}.{table_name}: {e}.")
+            df = pd.DataFrame()
+        
+    return df
